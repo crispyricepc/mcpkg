9b603d505bdbc35943168be282f56d2cc8c0b0a3
Ben Mitchell <bjosephmitchell@gmail.com>
2021-02-04 18:34:25 +0000

    Implement search function

--- mcpkg/cli.py
+++ mcpkg/cli.py
@@ -1,9 +1,11 @@
+from typing import Any
 from . import config
 from . import syncdb
 from .logger import log
 
 import argparse
 from colorama import Fore
+import re
 
 
 def add_subs(parser: argparse.ArgumentParser):
@@ -24,10 +26,21 @@ def add_subs(parser: argparse.ArgumentParser):
     list_parser.add_argument("-c", "--compact", help="compact output",
                              action="store_true")
 
+    search_parser = subparsers.add_parser("search",
+                                          help="search for a package")
+    search_parser.add_argument("expression", help="the package query")
+
 
 def add_opts(parser: argparse.ArgumentParser):
     parser.add_argument("-v", "--verbose", help="increase output verbosity",
                         action="store_true")
+
+
+def print_pack(pack: dict[str, Any], packname: str, compact: bool):
+    print(
+        f"{Fore.BLUE}{pack['display']}{Fore.RESET} ({Fore.GREEN}{packname}{Fore.RESET}) v.{pack['version']}")
+    if not compact:
+        print(f"\t{pack['description']}")
 
 
 def install():
@@ -45,11 +58,15 @@ def upgrade():
 def list(compact: bool):
     packlist = syncdb.get_local_pack_list()
     log("Listing packs:", "info")
-    for pack in packlist.keys():
-        print(
-            f"{Fore.BLUE}{packlist[pack]['display']}{Fore.RESET} ({Fore.GREEN}{pack}{Fore.RESET}) v.{packlist[pack]['version']}")
-        if not compact:
-            print(f"\t{packlist[pack]['description']}")
+    for packname in packlist.keys():
+        print_pack(packlist[packname], packname, compact)
+
+
+def search(expression: str):
+    packlist = syncdb.get_local_pack_list()
+    for packname in packlist.keys():
+        if re.search(expression, packname):
+            print_pack(packlist[packname], packname, False)
 
 
 def main():
@@ -71,3 +88,5 @@ def main():
         upgrade()
     elif args.command == "list":
         list(args.compact)
+    elif args.command == "search":
+        search(args.expression)
