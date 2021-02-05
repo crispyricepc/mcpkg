764303fa70394b957a6abe14601005b5edfccc08
Ben Mitchell <bjosephmitchell@gmail.com>
2021-02-05 10:41:13 +0000

    Move to an in-memory json load system

--- mcpkg/syncdb.py
+++ mcpkg/syncdb.py
@@ -1,14 +1,17 @@
-from . import config
-from .logger import log
-from .constants import LogLevel
-
-import tempfile
-import requests
 import json
-from typing import Any
-from tqdm import tqdm
-from colorama import Fore
+import re
+import tempfile
+from io import BytesIO
 from pathlib import Path
+from typing import Any
+
+import requests
+from colorama import Fore
+from tqdm import tqdm
+
+from . import config
+from .constants import LogLevel
+from .logger import log
 
 VT_URL = "https://vanillatweaks.net/"
 DP_URL = f"{VT_URL}assets/resources/json/{config.MC_BASE_VERSION}/dpcategories.json"
@@ -22,7 +25,7 @@ def formalise_name(name: str):
     """
 
 
-def dl_with_progress(url: str, dst: str, display: str):
+def dl_with_progress(url: str, display: str) -> BytesIO:
     # Streaming, so we can iterate over the response.
     response = requests.get(url, stream=True)
     total_size_in_bytes = int(response.headers.get('content-length', 0))
@@ -32,23 +35,24 @@ def dl_with_progress(url: str, dst: str, display: str):
                         bar_format="{l_bar}%s{bar}%s{r_bar}" %
                         (Fore.BLUE, Fore.RESET),
                         ascii=True)
-    with open(dst, 'wb') as file:
-        for data in response.iter_content(block_size):
-            progress_bar.update(len(data))
-            file.write(data)
+    buffer = BytesIO()
+    for data in response.iter_content(block_size):
+        progress_bar.update(len(data))
+        buffer.write(data)
     progress_bar.close()
-    log(f"'{url}' saved to '{dst}', status code {response.status_code}", LogLevel.DEBUG)
+    log(f"'{url}' saved to memory, status code {response.status_code}", LogLevel.DEBUG)
     if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
         log("Printing the fancy progress bar caused an issue, it's probably okay to ignore this", LogLevel.WARN)
+    return buffer
 
 
-def vt_to_packdb(src: Path, dst: Path):
+def vt_to_packdb(src: BytesIO, dst: Path) -> None:
     """
     Converts a vanilla tweaks JSON metadata file into a compatible list of packs.
     - src: The path to the vanilla tweaks metadata
     - dst: The path to the new pack list (usually ~/.config/mcpkg/packs.json)
     """
-    src_dict = json.load(src.open())
+    src_dict = json.load(src)
     new_dict = {}
     for src_category in src_dict["categories"]:
         for src_pack in src_category["packs"]:
@@ -68,24 +72,34 @@ def vt_to_packdb(src: Path, dst: Path):
     json.dump(new_dict, dst.open("w"), indent=2, sort_keys=True)
 
 
-def fetch_pack_list():
+def fetch_pack_list() -> None:
     """
     Fetches the pack list from Vanilla Tweaks servers and stores it in a compatible pack list
     """
-    file, filename = tempfile.mkstemp()
-    dl_with_progress(DP_URL, filename,
-                     f"[{Fore.GREEN}INFO{Fore.RESET}] Downloading datapack metadata")
-    vt_to_packdb(Path(filename), PACK_DB)
-    file, filename = tempfile.mkstemp()
-    dl_with_progress(
-        CT_URL, filename, f"[{Fore.GREEN}INFO{Fore.RESET}] Downloading crafting tweak metadata")
-    vt_to_packdb(Path(filename), PACK_DB)
+    datapack_metadata = dl_with_progress(DP_URL,
+                                         f"[{Fore.GREEN}INFO{Fore.RESET}] Downloading datapack metadata")
+    vt_to_packdb(datapack_metadata, PACK_DB)
+    tweak_metadata = dl_with_progress(
+        CT_URL, f"[{Fore.GREEN}INFO{Fore.RESET}] Downloading crafting tweak metadata")
+    vt_to_packdb(tweak_metadata, PACK_DB)
     log("Fetch complete", LogLevel.INFO)
 
 
-def get_local_pack_list() -> dict[str, dict[str, Any]]:
+def get_local_pack_list(pack_filter: list[str] = None) -> dict[str, dict[str, Any]]:
     if not PACK_DB.exists():
         log("Can't find a locally stored packdb.json. Attempting to fetch now...", LogLevel.WARN)
         fetch_pack_list()
 
-    return json.load(PACK_DB.open(mode="r"))
+    with PACK_DB.open() as file:
+        packs: dict = json.load(file)
+
+    if filter is not None:
+        results = {}
+        for expression in pack_filter:
+            for key in packs.keys():
+                if re.search(expression, key, re.IGNORECASE):
+                    results[key] = packs[key]
+    else:
+        results = packs
+
+    return results
