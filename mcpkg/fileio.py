8ef8ea43ae63a4ab56435f3bbadd2bde416c922e
Ben Mitchell <bjosephmitchell@gmail.com>
2021-04-21 19:59:30 +0100

    Improve the progress bar

--- mcpkg/fileio.py
+++ mcpkg/fileio.py
@@ -1,10 +1,10 @@
+from sys import stderr
+from colorama.ansi import Fore
 import requests
 from tempfile import mkdtemp
 from io import BytesIO
 from zipfile import ZipFile
 from pathlib import Path
-from colorama import Fore
-from tqdm import tqdm
 
 from . import syncdb
 from .constants import LogLevel, Pattern
@@ -15,22 +15,18 @@ from .pack import Pack, PackSet
 def dl_with_progress(url: str, display: str) -> BytesIO:
     # Streaming, so we can iterate over the response.
     response = requests.get(url, stream=True)
-    total_size_in_bytes = int(response.headers.get('content-length', 0))
+    total_downloaded = 0
+    total_size = int(response.headers.get('content-length', 0))
     block_size = 1024  # 1 Kibibyte
-    progress_bar = tqdm(total=total_size_in_bytes, unit="iB",
-                        unit_scale=True, desc=display,
-                        bar_format="{l_bar}%s{bar}%s{r_bar}" %
-                        (Fore.BLUE, Fore.RESET),
-                        ascii=True)
     buffer = BytesIO()
     for data in response.iter_content(block_size):
-        progress_bar.update(len(data))
+        total_downloaded += len(data)
+        print(
+            f"[{Fore.GREEN}INFO{Fore.RESET}] Downloading... [{round((total_downloaded / total_size) * 100, 1)}%]\r", end="", flush=True, file=stderr)
         buffer.write(data)
     buffer.seek(0)
-    progress_bar.close()
+    print()
     log(f"'{url}' saved to memory, status code {response.status_code}", LogLevel.DEBUG)
-    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
-        log("Printing the fancy progress bar caused an issue, it's probably okay to ignore this", LogLevel.WARN)
     return buffer
 
 
