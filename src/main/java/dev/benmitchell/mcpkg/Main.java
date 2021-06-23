4c88112270393c4785dcc428abbdc60648ca1b26
Ben Mitchell <bjosephmitchell@gmail.com>
2021-06-23 17:04:03 +0100

    Boilerplate starter code

---
+++ src/main/java/dev/benmitchell/mcpkg/Main.java
@@ -0,0 +1,11 @@
+package dev.benmitchell.mcpkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is mcpkg java edition");
+    }
+}
