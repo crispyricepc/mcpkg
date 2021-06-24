e90dbcf6f438611de5cc2cb374053cac1ef03a98
Ben Mitchell <bjosephmitchell@gmail.com>
2021-06-24 12:08:22 +0100

    Create stub implementations for Crafting, Data, and Resource Packs

---
+++ src/main/java/dev/benmitchell/mcpkg/packs/DataPack.java
@@ -0,0 +1,17 @@
+package dev.benmitchell.mcpkg.packs;
+
+import java.nio.file.Path;
+
+public abstract class DataPack implements Pack {
+    @Override
+    public void installTo(Path destination) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void install() {
+        // TODO Auto-generated method stub
+
+    }
+}
---
+++ src/main/java/dev/benmitchell/mcpkg/packs/ResourcePack.java
@@ -0,0 +1,17 @@
+package dev.benmitchell.mcpkg.packs;
+
+import java.nio.file.Path;
+
+public abstract class ResourcePack implements Pack {
+    @Override
+    public void installTo(Path destination) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void install() {
+        // TODO Auto-generated method stub
+
+    }
+}
