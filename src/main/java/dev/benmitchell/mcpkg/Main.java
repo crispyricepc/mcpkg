33b31e6200a9f29fc3437975bfed7629958cff47
Ben Mitchell <bjosephmitchell@gmail.com>
2021-07-01 21:57:47 +0100

    Simple testing files

--- src/main/java/dev/benmitchell/mcpkg/Main.java
+++ src/main/java/dev/benmitchell/mcpkg/Main.java
@@ -1,18 +1,28 @@
 package dev.benmitchell.mcpkg;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import dev.benmitchell.mcpkg.exceptions.MCPKGException;
 import dev.benmitchell.mcpkg.packs.Pack;
 import dev.benmitchell.mcpkg.sources.PackSource;
 import dev.benmitchell.mcpkg.vanillatweaks.VTSource;
 
 public class Main {
     public static void main(String[] args) {
-        PackSource src = new VTSource();
-        List<String> packsToGet = new ArrayList<String>();
-        packsToGet.add("slime");
-        List<Pack> packs = src.searchForPacks(packsToGet);
-        src.downloadPacks(packs);
+        try {
+            PackSource src = new VTSource();
+            List<String> packsToGet = new ArrayList<String>();
+            packsToGet.add("back");
+            List<Pack> packs = src.searchForPacks(packsToGet);
+            packs = src.downloadPacks(packs);
+            for (Pack pack : packs)
+                pack.install();
+        } catch (IOException ex) {
+            MCPKGLogger.err(ex);
+        } catch (MCPKGException ex) {
+            MCPKGLogger.err(ex);
+        }
     }
 }
