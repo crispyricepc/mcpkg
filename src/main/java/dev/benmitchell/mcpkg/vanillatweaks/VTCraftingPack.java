bf15b36892bbe94aa19bede660afbeb98bb5ac61
Ben Mitchell <bjosephmitchell@gmail.com>
2021-07-01 17:54:55 +0100

    Added install method to VT implementations

--- src/main/java/dev/benmitchell/mcpkg/vanillatweaks/VTCraftingPack.java
+++ src/main/java/dev/benmitchell/mcpkg/vanillatweaks/VTCraftingPack.java
@@ -4,6 +4,8 @@ import java.nio.file.Path;
 
 import org.json.simple.JSONObject;
 
+import dev.benmitchell.mcpkg.Platform;
+import dev.benmitchell.mcpkg.exceptions.InvalidDirectoryException;
 import dev.benmitchell.mcpkg.packs.PackType;
 
 public class VTCraftingPack extends VTPack {
@@ -15,5 +17,10 @@ public class VTCraftingPack extends VTPack {
     public void installTo(Path destination) {
         // TODO Auto-generated method stub
 
+    }
+
+    @Override
+    public void install() throws InvalidDirectoryException {
+        installTo(Platform.getDataPacksDir());
     }
 }
--- src/main/java/dev/benmitchell/mcpkg/vanillatweaks/VTDataPack.java
+++ src/main/java/dev/benmitchell/mcpkg/vanillatweaks/VTDataPack.java
@@ -4,6 +4,8 @@ import java.nio.file.Path;
 
 import org.json.simple.JSONObject;
 
+import dev.benmitchell.mcpkg.Platform;
+import dev.benmitchell.mcpkg.exceptions.InvalidDirectoryException;
 import dev.benmitchell.mcpkg.packs.PackType;
 
 public class VTDataPack extends VTPack {
@@ -14,6 +16,12 @@ public class VTDataPack extends VTPack {
     @Override
     public void installTo(Path destination) {
         // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void install() throws InvalidDirectoryException {
+        installTo(Platform.getDataPacksDir());
 
     }
 }
