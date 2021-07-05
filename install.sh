8291924e9183bc2fed04d1c44f64afcb14e53e9e
Ben Mitchell <bjosephmitchell@gmail.com>
2021-07-05 12:54:01 +0100

    New install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+MCPKG_VERSION=0.1
+BIN_FILE=$HOME/.local/bin/mcpkg
+LIB_FILE=$HOME/.local/lib/mcpkg/mcpkg-$MCPKG_VERSION.jar
+
+mkdir -p $HOME/.local/lib/mcpkg
+
+echo "Running build..."
+
+mvn -B compile assembly:single
+
+echo "Installing mcpkg to $LIB_FILE and $BIN_FILE..."
+
+echo -e "#!/bin/sh\n\
+java -jar $LIB_FILE \"\$@\"\n" > $BIN_FILE
+chmod +x $BIN_FILE
+cp target/mcpkg-$MCPKG_VERSION-dev-jar-with-dependencies.jar $LIB_FILE
+
+echo "Done"
--- mcpkg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-TARGET=target/mcpkg-0.1-dev-jar-with-dependencies.jar
-
-java -jar $TARGET "$@"
