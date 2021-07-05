87ee26a0344b5037fd56d95be50fe3d77b1f9cd1
Ben Mitchell <bjosephmitchell@gmail.com>
2021-07-05 16:14:37 +0100

    Update version

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MCPKG_VERSION=0.1
+MCPKG_VERSION=2.0-dev
 BIN_FILE=$HOME/.local/bin/mcpkg
 LIB_FILE=$HOME/.local/lib/mcpkg/mcpkg-$MCPKG_VERSION.jar
 
@@ -15,6 +15,6 @@ echo "Installing mcpkg to $LIB_FILE and $BIN_FILE..."
 echo -e "#!/bin/sh\n\
 java -jar $LIB_FILE \"\$@\"\n" > $BIN_FILE
 chmod +x $BIN_FILE
-cp target/mcpkg-$MCPKG_VERSION-dev-jar-with-dependencies.jar $LIB_FILE
+cp target/mcpkg-$MCPKG_VERSION-jar-with-dependencies.jar $LIB_FILE
 
 echo "Done"
